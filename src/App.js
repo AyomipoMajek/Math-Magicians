37613ac55253979e74152f61a88ab1bf3a9d100e
Ayomipo Majek <ayomipomajek@gmail.com>
2023-02-22 20:32:10 +0100

    linters fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
 import Nav from './component/Nav';
 import Quote from './component/Quote';
-import Calculator from './component/Calculator';
+import Calculator from './component/calculator';
 
 function App() {
   return (
